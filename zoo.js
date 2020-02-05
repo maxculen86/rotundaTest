9a09c82d7fbabb86082a068afc15b7051ec2cdf6
maxculen86 <42255662+maxculen86@users.noreply.github.com>
2020-02-05 16:23:01 -0300

    Add files via upload

--- zoo.js
+++ zoo.js
@@ -1,26 +1,30 @@
 class Animal {
+    constructor(pharse) {
+        this.catchphrase = pharse;
+    }
+ 
     speak(words) {
-        console.log(this.translate(words, ""));
+        console.log(this.translate(words, catchphrase));
     }
     translate(words, catchphrase ) {
         return words.replace(/ /g, catchphrase).concat(catchphrase);
     }
-  }
+}
 
 class Lion extends Animal {
     speak(words) {
-        console.log(this.translate(words, " roar "));
+        console.log(this.translate(words, this.catchphrase));
     }
 }
 
 class Tiger extends Animal {
     speak(words) {
-        console.log(this.translate(words, " grrr "));
+        console.log(this.translate(words, this.catchphrase));
     }
 }
 
-let tiger = new Tiger();
-let lion = new Lion();
+let tiger = new Tiger(" grrr ");
+let lion = new Lion(" roar ");
 
 lion.speak("I'm a lion");
 tiger.speak("Lions suck");
